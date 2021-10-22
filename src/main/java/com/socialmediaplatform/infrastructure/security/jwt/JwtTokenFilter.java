fde154b3524516ca956e61713719bd0c73a2dbe3
KamilB00 <bonkowski.kamil@icloud.com>
2021-10-22 11:00:12 +0200

    JWT Token filter

--- src/main/java/com/socialmediaplatform/infrastructure/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/socialmediaplatform/infrastructure/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,45 @@
 package com.socialmediaplatform.infrastructure.security.jwt;
 
-public class JwtTokenFilter {
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if(token != null){
+            try{
+                if(jwtTokenProvider.validateToken(token)){
+                    Authentication auth = jwtTokenProvider.getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }catch (JwtException | UsernameNotFoundException ex){
+                SecurityContextHolder.clearContext();
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                response.setContentType("text/plain");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(ex.getMessage());
+                return;
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
 }
